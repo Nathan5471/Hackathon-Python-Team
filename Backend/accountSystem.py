f3c7336fa961505510b881bae0123cd158fb805e
Nathan <nathanc4571@gmail.com>
2023-12-20 11:12:49 -0500

    account system

---
+++ Backend/accountSystem.py
@@ -0,0 +1,97 @@
+import sqlite3 as sql
+import hashlib
+import os
+
+def createAccount(username, email, password):
+    salt = os.urandom(32)
+    hashedPassword = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    connection = sql.connect('accountSystem.db')
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO accounts VALUES (?, ?, ?, ?)', (username, email, hashedPassword, salt))
+    connection.commit()
+    connection.close()
+    
+def checkAccount(username, password):
+    connection = sql.connect('accountSystem.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM accounts WHERE username=?', (username,))
+    account = cursor.fetchone()
+    connection.close()
+    if account == None:
+        return False
+    salt = account[3]
+    hashedPassword = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    if hashedPassword == account[2]:
+        return True
+    else:
+        return False
+    
+def changePassword(username, oldPassword, newPassword):
+    connection = sql.connect('accountSystem.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM accounts WHERE username=?', (username,))
+    account = cursor.fetchone()
+    connection.close()
+    if account == None:
+        return False
+    salt = account[3]
+    hashedPassword = hashlib.pbkdf2_hmac('sha256', oldPassword.encode('utf-8'), salt, 100000)
+    if hashedPassword == account[2]:
+        salt = os.urandom(32)
+        hashedPassword = hashlib.pbkdf2_hmac('sha256', newPassword.encode('utf-8'), salt, 100000)
+        connection = sql.connect('accountSystem.db')
+        cursor = connection.cursor()
+        cursor.execute('UPDATE accounts SET password=?, salt=? WHERE username=?', (hashedPassword, salt, username))
+        connection.commit()
+        connection.close()
+        return True
+    else:
+        return False
+    
+def changeEmail(username, password, newEmail):
+    connection = sql.connect('accountSystem.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM accounts WHERE username=?', (username,))
+    account = cursor.fetchone()
+    connection.close()
+    if account == None:
+        return False
+    salt = account[3]
+    hashedPassword = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    if hashedPassword == account[2]:
+        connection = sql.connect('accountSystem.db')
+        cursor = connection.cursor()
+        cursor.execute('UPDATE accounts SET email=? WHERE username=?', (newEmail, username))
+        connection.commit()
+        connection.close()
+        return True
+    else:
+        return False
+    
+def deleteAccount(username, password):
+    connection = sql.connect('accountSystem.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM accounts WHERE username=?', (username,))
+    account = cursor.fetchone()
+    connection.close()
+    if account == None:
+        return False
+    salt = account[3]
+    hashedPassword = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    if hashedPassword == account[2]:
+        connection = sql.connect('accountSystem.db')
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM accounts WHERE username=?', (username,))
+        connection.commit()
+        connection.close()
+        return True
+    else:
+        return False
+
+def getAccount(username):
+    connection = sql.connect('accountSystem.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM accounts WHERE username=?', (username,))
+    account = cursor.fetchone()
+    connection.close()
+    return account
