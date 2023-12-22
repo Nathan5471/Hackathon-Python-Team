77e089af8a48559448f41f2f94a7fc1e44a5d04e
Nathan <nathanc4571@gmail.com>
2023-12-22 18:57:02 -0500

    account menu

--- main.py
+++ main.py
@@ -13,14 +13,22 @@ def gradient(canvas, color1, color2, width):
 
 def buttonColor(event, button, color):
     button.config(bg=color)
+    
+def usernameButtonPressed():
+    if usernameMenu.winfo_ismapped():
+        usernameMenu.place_forget()
+    else:
+        usernameMenu.place(x=0, y=50)
 
 window = tk.Tk()
 window.title("Elearning")
 window.geometry("600x400")
 
+#Creates the main page for the application
 canvas = tk.Canvas(window, width=600, height=400)
 canvas.place(x=0, y=0)
 
+#The top bar for the application
 topBar = tk.Canvas(window, width=600, height=50)
 topBar.place(x=0, y=0)
 usernameButton = tk.Button(topBar, text="Username", relief=tk.FLAT, width=10, height=1, background="#c1b4f2")
@@ -29,7 +37,12 @@ usernameButton.bind("<Enter>", lambda event: buttonColor(event, usernameButton, 
 usernameButton.bind("<Leave>", lambda event: buttonColor(event, usernameButton, "#c1b4f2"))
 usernameButton.place(x=0, y=0)
 
+#The account management menu
+#Accesed by pressing the username button
+usernameMenu = tk.Canvas(window, width=100, height=100)
+usernameMenu.place(x=0, y=50)
 
+#Manages the gradients
 window.update()  # needed for winfo_height() to work
 gradient(canvas, '#D8B5FF', '#1EAE98', 600)
 gradient(topBar, '#D8B5FF', '#c1b4f2', 600)
