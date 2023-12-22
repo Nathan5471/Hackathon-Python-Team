605e9798e554980c262b75e17a8a8e24a0a4c56f
Nathan <nathanc4571@gmail.com>
2023-12-22 18:35:15 -0500

    reduced call of .winfo_height()

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import tkinter as tk
 def gradient(canvas, color1, color2, width):
     r1, g1, b1 = int(color1[1:3], 16), int(color1[3:5], 16), int(color1[5:7], 16)
     r2, g2, b2 = int(color2[1:3], 16), int(color2[3:5], 16), int(color2[5:7], 16)
-    for i in range(canvas.winfo_height()):
-        r = r1 * (1 - (i / canvas.winfo_height())) + r2 * (i / canvas.winfo_height())
-        g = g1 * (1 - (i / canvas.winfo_height())) + g2 * (i / canvas.winfo_height())
-        b = b1 * (1 - (i / canvas.winfo_height())) + b2 * (i / canvas.winfo_height())
+    canvasHeight = canvas.winfo_height()
+    for i in range(canvasHeight):
+        r = r1 * (1 - (i / canvasHeight)) + r2 * (i / canvasHeight)
+        g = g1 * (1 - (i / canvasHeight)) + g2 * (i / canvasHeight)
+        b = b1 * (1 - (i / canvasHeight)) + b2 * (i / canvasHeight)
         color = '#%02x%02x%02x' % (round(r), round(g), round(b))
         canvas.create_line(0, i, width, i, fill=color)
 
