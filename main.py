44dfab90ff576e97d5eb86211b927ef64025bb1c
Nathan <nathanc4571@gmail.com>
2023-12-22 18:31:29 -0500

    fixed gradient

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 
 def gradient(canvas, color1, color2, width):
+    r1, g1, b1 = int(color1[1:3], 16), int(color1[3:5], 16), int(color1[5:7], 16)
+    r2, g2, b2 = int(color2[1:3], 16), int(color2[3:5], 16), int(color2[5:7], 16)
     for i in range(canvas.winfo_height()):
-        color = (int(color1[1:3], 16) * (1 - (i / canvas.winfo_height())) + int(color2[1:3], 16) * (i / canvas.winfo_height()))
-        color = round(color)
-        color = '#%02x%02x%02x' % (color, color, color)
+        r = r1 * (1 - (i / canvas.winfo_height())) + r2 * (i / canvas.winfo_height())
+        g = g1 * (1 - (i / canvas.winfo_height())) + g2 * (i / canvas.winfo_height())
+        b = b1 * (1 - (i / canvas.winfo_height())) + b2 * (i / canvas.winfo_height())
+        color = '#%02x%02x%02x' % (round(r), round(g), round(b))
         canvas.create_line(0, i, width, i, fill=color)
 
 window = tk.Tk()
